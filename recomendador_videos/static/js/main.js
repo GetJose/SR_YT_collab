9361d3378def7c5556cf5a963bf652a48985e599
Getulio <getulio.santos@ufvjm.edu.br>
2024-09-30 08:46:46 -0300

    saving the initial assessment and opening the video in full screen

--- recomendador_videos/static/js/main.js
+++ recomendador_videos/static/js/main.js
@@ -1,11 +1,9 @@
-// Função para pegar o CSRF token dos cookies
 function getCookie(name) {
     let cookieValue = null;
     if (document.cookie && document.cookie !== '') {
         const cookies = document.cookie.split(';');
         for (let i = 0; i < cookies.length; i++) {
             const cookie = cookies[i].trim();
-            // Se este cookie começa com o nome que desejamos
             if (cookie.substring(0, name.length + 1) === (name + '=')) {
                 cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
                 break;
@@ -17,41 +15,72 @@ function getCookie(name) {
 
 const csrftoken = getCookie('csrftoken');
 
-// Configura o jQuery para adicionar o token CSRF automaticamente às requisições AJAX
 $.ajaxSetup({
     beforeSend: function(xhr, settings) {
         if (!(/^http:.*/.test(settings.url) || /^https:.*/.test(settings.url))) {
-            // Apenas adiciona o token para requisições que vão para o mesmo domínio
             xhr.setRequestHeader("X-CSRFToken", csrftoken);
         }
     }
 });
 
-$(document).on('click', '.rate-button', function(e) {
-    e.preventDefault();
+document.addEventListener("DOMContentLoaded", function () {
+    $(document).on('click', '.video-item', function(e) {
+        e.preventDefault();
+        
+        const videoId = $(this).attr('id').split('-')[1];  
+        const url = '/initialRateVideo/';  
 
-    var videoId = $(this).data('video-id');
-    var rating = $(this).data('rating');
-    var url = $(this).data('url');  // Pega a URL diretamente do data-attribute
-
-    $.ajax({
-        url: url,
-        method: 'POST',
-        data: {
-            'rating': rating
-        },
-        success: function(response) {
-            if (rating === 1) {
-                $('#video-' + videoId + ' .rate-button').removeClass('liked disliked');
-                $('#video-' + videoId + ' .rate-button[data-rating="1"]').addClass('liked');
-            } else {
-                $('#video-' + videoId + ' .rate-button').removeClass('liked disliked');
-                $('#video-' + videoId + ' .rate-button[data-rating="-1"]').addClass('disliked');
+        $.ajax({
+            url: url,
+            method: 'POST',
+            data: {
+                'video_id': videoId,
+                'rating': 0  
+            },
+            success: function(response) {
+                console.log('Avaliação inicial salva com sucesso.');
+            },
+            error: function(xhr) {
+                console.error('Erro ao salvar a avaliação inicial.');
             }
-            $('#video-' + videoId + ' .rating-message').text(response.message);
-        },
-        error: function(xhr) {
-            $('#video-' + videoId + ' .rating-message').text('Erro ao avaliar o vídeo.');
+        });
+
+        const iframe = $(this).find('iframe')[0];
+        if (iframe) {
+            let requestFullScreen = iframe.requestFullscreen || iframe.mozRequestFullScreen || iframe.webkitRequestFullscreen || iframe.msRequestFullscreen;
+            if (requestFullScreen) {
+                requestFullScreen.call(iframe);
+            }
         }
+    });
+
+    $(document).on('click', '.rate-button', function(e) {
+        e.preventDefault();
+        e.stopPropagation();
+        const videoId = $(this).data('video-id');
+        const rating = $(this).data('rating');
+        const url = $(this).data('url');
+        const messageDiv = $(this).siblings('.rating-message');
+
+        $.ajax({
+            url: url,
+            method: 'POST',
+            data: {
+                'rating': rating
+            },
+            success: function(response) {
+                if (rating === 1) {
+                    $('#video-' + videoId + ' .rate-button').removeClass('liked disliked');
+                    $('#video-' + videoId + ' .rate-button[data-rating="1"]').addClass('liked');
+                } else {
+                    $('#video-' + videoId + ' .rate-button').removeClass('liked disliked');
+                    $('#video-' + videoId + ' .rate-button[data-rating="-1"]').addClass('disliked');
+                }
+                messageDiv.text(response.message);
+            },
+            error: function(xhr) {
+                messageDiv.text('Erro ao avaliar o vídeo.');
+            }
+        });
     });
 });
