1715968dba11f89ed6f9dba1c7dcc5d9478d9836
Getulio <getulio.santos@ufvjm.edu.br>
2025-02-18 09:34:37 -0300

    changing the way playlists are listed, and the way videos are sent to them

--- recomendador_videos/playlists/views/enviar_video.py
+++ recomendador_videos/playlists/views/enviar_video.py
@@ -4,13 +4,16 @@ from django.views import View
 from django.http import HttpResponseBadRequest
 from ..models import Playlist, PlaylistVideo
 from recomendador_videos.youtube_integration.models import Video
+from django.db.models import Max
 
 class EnviarVideoParaPlaylistView(LoginRequiredMixin, View):
     template_name = 'apps/playlists/enviar_video.html'
 
     def get(self, request, video_id):
         video = get_object_or_404(Video, youtube_id=video_id)
-        playlists = Playlist.objects.filter(usuario=request.user)
+
+        # Filtra apenas playlists que AINDA NÃO contêm o vídeo
+        playlists = Playlist.objects.filter(usuario=request.user).exclude(videos__video=video)
 
         return render(request, self.template_name, {
             'video': video,
@@ -25,6 +28,13 @@ class EnviarVideoParaPlaylistView(LoginRequiredMixin, View):
             return HttpResponseBadRequest("Nenhuma playlist selecionada.")
 
         playlist = get_object_or_404(Playlist, id=playlist_id, usuario=request.user)
-        PlaylistVideo.objects.create(playlist=playlist, video=video)
+
+        # Verifica a última posição atual da playlist
+        ultima_ordem = PlaylistVideo.objects.filter(playlist=playlist).aggregate(Max('ordem'))['ordem__max']
+        nova_ordem = (ultima_ordem + 1) if ultima_ordem is not None else 1  # Se não houver vídeos, começa com 1
+
+        # Adiciona o vídeo SOMENTE se ainda não estiver na playlist
+        if not PlaylistVideo.objects.filter(playlist=playlist, video=video).exists():
+            PlaylistVideo.objects.create(playlist=playlist, video=video, ordem=nova_ordem)
 
         return redirect('detalhe_playlist', playlist.id)
--- recomendador_videos/playlists/views/lista.py
+++ recomendador_videos/playlists/views/lista.py
@@ -2,9 +2,33 @@ from django.shortcuts import render
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views import View
 from ..models import Playlist
+from django.db.models import Q
+from recomendador_videos.recomendacao.services.similaridade import calcular_similaridade_usuarios 
 
 class ListaPlaylistsView(LoginRequiredMixin, View):
     template_name = 'apps/playlists/lista_playlists.html'
+
     def get(self, request):
-        playlists = Playlist.objects.all()
-        return render(request, self.template_name, {'playlists': playlists})
+        query = request.GET.get("q", "")
+        usuario = request.user
+
+        # 1️⃣ Playlists do próprio usuário
+        minhas_playlists = Playlist.objects.filter(usuario=usuario)
+
+        # 2️⃣ Pesquisar playlists por nome ou descrição (todas as playlists do BD)
+        if query:
+            playlists_pesquisa = Playlist.objects.filter(Q(nome__icontains=query) | Q(descricao__icontains=query))
+        else:
+            playlists_pesquisa = Playlist.objects.none()
+
+        # 3️⃣ Playlists de usuários semelhantes
+        similaridade = calcular_similaridade_usuarios(usuario)
+        usuarios_similares = list(similaridade.keys())[:3] 
+        playlists_similares = Playlist.objects.filter(usuario__in=usuarios_similares)
+
+        return render(request, self.template_name, {
+            "minhas_playlists": minhas_playlists,
+            "playlists_pesquisa": playlists_pesquisa,
+            "playlists_similares": playlists_similares,
+            "query": query
+        })
